4333fdd2060b6a5427238197487910235bb8b61c
Hamel Patel <hamel.patel@kcl.ac.uk>
2015-07-02 16:10:38 +0100

    unhashed variables and specified file locations

--- bin/create_summary_report.sh
+++ bin/create_summary_report.sh
@@ -25,11 +25,11 @@ multimapping_snp_file=${2}
 ##	SNP/SAMPLE NUMBER BEFORE QC
 ##
 
-#total_snp_number_prior_to_qc_temp=$(wc -l ${report_file}.report | awk '{print $1}')
+total_snp_number_prior_to_qc_temp=$(wc -l ${report_file}.report | awk '{print $1}')
 
-#total_snp_number_prior_to_qc=$(echo $(( $total_snp_number_prior_to_qc_temp - 1)))
+total_snp_number_prior_to_qc=$(echo $(( $total_snp_number_prior_to_qc_temp - 1)))
 
-#total_sample_number_prior_to_qc=$(head -1 ${report_file}.report | sed s'/\t/\n/g' | grep GType | wc -l)
+total_sample_number_prior_to_qc=$(head -1 ${report_file}.report | sed s'/\t/\n/g' | grep GType | wc -l)
 
 ##
 ##	SNP/SAMPLE NUMBER AFTER QC
@@ -94,33 +94,33 @@ echo -e "\n*************************************************Summary Report******
 
 echo -e "\nPlease note: unless specified, any Het or related samples have not been removed, but only identified for users awareness.
 Duplicate samples, if any, have been changed to a unique id run through the QC pipeline and converted back to original form.
-All temp ID changes are listed in '"${report_file}.report_duplicate_sample_id_temp_changes"'
-SNP probes which target multiple regions of the genome have been identified and removed. These SNP IDs are found in '"${multimapping_snp_file}"'
-All allele have been converted to Illumina top strand" >> README_summary_report
+All temp ID changes are listed in '"sample_information/${report_file}.report_duplicate_sample_id_temp_changes"'
+SNP probes which target multiple regions of the genome have been identified and removed. These SNP IDs are found in '"snp_information/${multimapping_snp_file}"'
+All alleles have been converted to Illumina top strand" >> README_summary_report
 
-echo -e "\nA summary of the pipeline is given below:" >> README_summary_report
+echo -e "\nA summary of the pipeline analysis is given below:" >> README_summary_report
 
-echo -e "\nTotal number of SNPs prior QC: $total_snp_number_after_qc" >> README_summary_report
+echo -e "\nTotal number of SNPs prior QC: $total_snp_number_prior_to_qc" >> README_summary_report
 
-echo -e "\nTotal number of Samples prior to QC: $total_sample_number_after_qc" >> README_summary_report
+echo -e "\nTotal number of Samples prior to QC: $total_sample_number_prior_to_qc" >> README_summary_report
 
-echo -e "\nTotal number of mulimapping SNPs removed: $number_of_multimapping_snps		...see file: '"${multimapping_snp_file}"' for SNP IDs" >> README_summary_report
+echo -e "\nTotal number of mulimapping SNPs removed: $number_of_multimapping_snps		...see file: '"snp_information/${multimapping_snp_file}"' for SNP IDs" >> README_summary_report
 
-echo -e "\nTotal number of SNPs removed due to failing genomestudio stage: $number_of_snps_failed_genomestudio		...see file: '"SNP_zeroed_in_genomestudio_to_remove"' for SNP IDs" >> README_summary_report
+echo -e "\nTotal number of SNPs removed due to failing genomestudio stage: $number_of_snps_failed_genomestudio		...see file: '"snp_information/SNP_zeroed_in_genomestudio_to_remove"' for SNP IDs" >> README_summary_report
 
-echo -e "\nNumber of samples removed due to call rate below 98%: $number_of_samples_with_low_call_rate			...see file: '"samples_with_low_callrate_to_exclude"' for sample IDs " >> README_summary_report
+echo -e "\nNumber of samples removed due to call rate below 98%: $number_of_samples_with_low_call_rate			...see file: '"sample_information/samples_with_low_callrate_to_exclude"' for sample IDs " >> README_summary_report
 
 echo -e "\nTotal number of SNPs after QC: $total_snp_number_after_qc" >> README_summary_report
 
 echo -e "\nTotal number of Samples after QC: $total_sample_number_after_qc" >> README_summary_report
 
-echo -e "\nNumber of duplicate samples: $number_of_duplicate_samples		...see file: '"${report_file}.report_duplicate_sample_id_temp_changes"' for sample IDs " >> README_summary_report
+echo -e "\nNumber of duplicate samples: $number_of_duplicate_samples		...see file: '"sample_information/${report_file}.report_duplicate_sample_id_temp_changes"' for sample IDs " >> README_summary_report
 
-echo -e "\nNumber of samples to be aware of due to heterozygote variability: $number_of_het_samples		...see file '"het_outliers_sample_exclude"' for sample IDs" >> README_summary_report
+echo -e "\nNumber of samples to be aware of due to heterozygote variability: $number_of_het_samples		...see file '"sample_information/het_outliers_sample_exclude"' for sample IDs" >> README_summary_report
 
-echo -e "\nNumber of related samples identified: $number_of_related_samples		...see file '"related_sample_exclude"' for sample IDs" >> README_summary_report
+echo -e "\nNumber of related samples identified: $number_of_related_samples		...see file '"sample_information/related_sample_exclude"' for sample IDs" >> README_summary_report
 
-echo -e "\nNumber of gender missmatches identified: $number_of_gender_missmatches		...see file '"gender_missmatches"' for sample IDs" >> README_summary_report
+echo -e "\nNumber of gender missmatches identified: $number_of_gender_missmatches		...see file '"sample_information/gender_missmatches"' for sample IDs" >> README_summary_report
 
 echo -e "\nTotal number of SNPs after QC: $total_snp_number_after_qc" >> README_summary_report
 
